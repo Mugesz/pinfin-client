2aefd1e958952c90fca8cc5e3eef15f353163b68
frekerzzzz <kannamugesh135@gmail.com>
2024-04-04 16:34:15 +0530

    span

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,6 @@ const Header = () => {
   return (
    <nav className="navbar navbar-expand-lg  bg">
   <Link to="/" className="navbar-brand text-light" href="#">All Applicants</Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon" />
-  </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
