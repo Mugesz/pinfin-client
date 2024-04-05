74c1bef8bcc8e0bf7e6683e6ef4b3b4db2584ef1
frekerzzzz <kannamugesh135@gmail.com>
2024-04-05 11:20:37 +0530

    responsive sadded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
    <nav className="navbar navbar-expand-lg  bg">
   <Link to="/" className="navbar-brand text-light" href="#">All Applicants</Link>
-  <div className="collapse navbar-collapse" id="navbarNav">
+  
     <ul className="navbar-nav">
       <li className="nav-item active">
         <Link to={"/add-applicants"} className="nav-link text-light" href="#">Create Applicants</Link>
       </li>
     </ul>
-  </div>
+
 </nav>
 
   )
